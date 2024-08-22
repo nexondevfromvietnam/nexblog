2d2a4810fa6dce6136ff5d1867b2b9a9175b521e
nexondevfromvietnam <khorryvfahnifa@tmt.vitik.edu.vn>
2024-08-22 11:39:17 +0700

    Update main.js
    update https

--- main.js
+++ main.js
@@ -273,6 +273,12 @@ app.get("/home", async (req, res) => {
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
                 <link href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
                 <link rel="icon" href="./img/a.jpg" type="image"/>
+                <script>
+                  var host = "ndqm.site";
+                  if ((host == window.location.host) && (window.location.protocol != "https:")){
+                    window.location.protocol = "https";
+                  }
+                </script>
             </head>
             <body>
                 <h1 style="text-align: center;">NEXBlog | Home</h1><p style="position: fixed; float: right; bottom: 2vh; right: 2vw;"><a href="/login">Login/Register</a></p>
